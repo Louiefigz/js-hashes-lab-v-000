9b9b09c7ef40183713d1c41511f483f190f6c183
Louiefigz <figueroa.aaron@gmail.com>
2016-04-25 12:32:06 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function addIngredient(recipe, item, measure){
+  recipe[item] = measure;
+  return recipe;
+}
+
+
+function removeIngredient(recipe){
+
+  delete recipe["vanilla"];
+  return recipe;
+}
+
+function updateIngredient(recipe, item, measure){
+  recipe[item] = measure;
+  return recipe;
+}
+
+function readRecipe(recipe){
+
+  for (var item in recipe){
+  console.log("this recipe calls for " + recipe[item] + " of " + item );
+  }
+}
